b7cbb1ae280fcc288f94432773708efbb96bcd97
Sam Russell <sam.russell@powershop.co.nz>
2015-11-02 15:22:50 +1300

    http response ready

--- lib/http_response.rb
+++ lib/http_response.rb
@@ -1,4 +1,9 @@
 class HttpResponse
+  def initialize(file_data)
+    @file_data = file_data
+  end
+
   def to_string
+    "HTTP/1.0 200 OK\r\nContent-Type: text-html\r\n\r\n" + @file_data
   end
 end
--- spec/http_response_spec.rb
+++ spec/http_response_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 require './lib/http_response'
 
 describe HttpResponse do
+  let(:file_data) { "fake file data\ndo not care\n" }
+  let(:expected_response) { "HTTP/1.0 200 OK\r\nContent-Type: text-html\r\n\r\n" + file_data }
+  let(:http_response) { HttpResponse.new(file_data) }
+
+  describe '#to_string' do
+    subject { http_response.to_string }
+    it { is_expected.to eq(expected_response) }
+  end
 end
